601ba8b3e65cc99f5f46a28c5303eef867844d64
Radekeo <66294398+Radekeo@users.noreply.github.com>
2021-09-22 10:47:21 +0100

    contains result from scanning user's image input

---
+++ scan.py
@@ -0,0 +1,20 @@
+import easyocr
+from helpers import nigeria
+
+# read image file
+IMAGE_PATH = 'passport4.jpg'
+
+# extract text from image
+reader = easyocr.Reader(['en'], gpu=False)
+result = reader.readtext(IMAGE_PATH)
+# print(result)
+
+# store useful values extracted from image in lists
+places = []
+strr = []
+
+for value in result:
+    if value[1] in nigeria:     # get values that have Nigerian states or cities
+        places.append(value[1])
+    elif '<' in value[1]:       # get values at the bottom of the image
+        strr.append(value[1])
